37b247918b06d14259ec49dcfd3f6504e5a80f1e
Chenghao-Tan <tch888free@gmail.com>
2023-09-05 22:07:43 +0800

    feat: Apply rateLimiter to all

--- router/router.go
+++ router/router.go
@@ -28,41 +28,41 @@ func NewRouter() *gin.Engine {
 
 		userAPI := rootAPI.Group("user")
 		{
-			userAPI.POST("/register/", midware.MiddlewareRateLimitWithRedis(rateLimit, time.Second), api.POSTUserRegister) // 应用限流中间件
-			userAPI.POST("/login/", midware.MiddlewareRateLimitWithRedis(rateLimit, time.Second), api.POSTUserLogin)       // 应用限流中间件
-			userAPI.GET("/", midware.MiddlewareAuth(true), api.GETUserInfo)                                                // 应用jwt鉴权中间件(强制)
+			userAPI.POST("/register/", midware.MiddlewareRateLimitWithRedis(rateLimit, time.Second), api.POSTUserRegister)                // 应用限流中间件
+			userAPI.POST("/login/", midware.MiddlewareRateLimitWithRedis(rateLimit, time.Second), api.POSTUserLogin)                      // 应用限流中间件
+			userAPI.GET("/", midware.MiddlewareRateLimitWithRedis(rateLimit, time.Second), midware.MiddlewareAuth(true), api.GETUserInfo) // 应用限流中间件, jwt鉴权中间件(强制)
 		}
 
 		publishAPI := rootAPI.Group("publish")
 		{
-			publishAPI.POST("/action/", midware.MiddlewareAuth(true), api.POSTPublish)  // 应用jwt鉴权中间件(强制)
-			publishAPI.GET("/list/", midware.MiddlewareAuth(false), api.GETPublishList) // 应用jwt鉴权中间件
+			publishAPI.POST("/action/", midware.MiddlewareRateLimitWithRedis(rateLimit, time.Second), midware.MiddlewareAuth(true), api.POSTPublish)  // 应用限流中间件, jwt鉴权中间件(强制)
+			publishAPI.GET("/list/", midware.MiddlewareRateLimitWithRedis(rateLimit, time.Second), midware.MiddlewareAuth(false), api.GETPublishList) // 应用限流中间件, jwt鉴权中间件
 		}
 
 		favoriteAPI := rootAPI.Group("favorite")
 		{
-			favoriteAPI.POST("/action/", midware.MiddlewareAuth(true), api.POSTFavorite)  // 应用jwt鉴权中间件(强制)
-			favoriteAPI.GET("/list/", midware.MiddlewareAuth(false), api.GETFavoriteList) // 应用jwt鉴权中间件
+			favoriteAPI.POST("/action/", midware.MiddlewareRateLimitWithRedis(rateLimit, time.Second), midware.MiddlewareAuth(true), api.POSTFavorite)  // 应用限流中间件, jwt鉴权中间件(强制)
+			favoriteAPI.GET("/list/", midware.MiddlewareRateLimitWithRedis(rateLimit, time.Second), midware.MiddlewareAuth(false), api.GETFavoriteList) // 应用限流中间件, jwt鉴权中间件
 		}
 
 		commentAPI := rootAPI.Group("comment")
 		{
-			commentAPI.POST("/action/", midware.MiddlewareAuth(true), api.POSTComment)  // 应用jwt鉴权中间件(强制)
-			commentAPI.GET("/list/", midware.MiddlewareAuth(false), api.GETCommentList) // 应用jwt鉴权中间件
+			commentAPI.POST("/action/", midware.MiddlewareRateLimitWithRedis(rateLimit, time.Second), midware.MiddlewareAuth(true), api.POSTComment)  // 应用限流中间件, jwt鉴权中间件(强制)
+			commentAPI.GET("/list/", midware.MiddlewareRateLimitWithRedis(rateLimit, time.Second), midware.MiddlewareAuth(false), api.GETCommentList) // 应用限流中间件, jwt鉴权中间件
 		}
 
 		relationAPI := rootAPI.Group("relation")
 		{
-			relationAPI.POST("/action/", midware.MiddlewareAuth(true), api.POSTFollow)             // 应用jwt鉴权中间件(强制)
-			relationAPI.GET("/follow/list/", midware.MiddlewareAuth(false), api.GETFollowList)     // 应用jwt鉴权中间件
-			relationAPI.GET("/follower/list/", midware.MiddlewareAuth(false), api.GETFollowerList) // 应用jwt鉴权中间件
-			relationAPI.GET("/friend/list/", midware.MiddlewareAuth(true), api.GETFriendList)      // 应用jwt鉴权中间件(强制)
+			relationAPI.POST("/action/", midware.MiddlewareRateLimitWithRedis(rateLimit, time.Second), midware.MiddlewareAuth(true), api.POSTFollow)             // 应用限流中间件, jwt鉴权中间件(强制)
+			relationAPI.GET("/follow/list/", midware.MiddlewareRateLimitWithRedis(rateLimit, time.Second), midware.MiddlewareAuth(false), api.GETFollowList)     // 应用限流中间件, jwt鉴权中间件
+			relationAPI.GET("/follower/list/", midware.MiddlewareRateLimitWithRedis(rateLimit, time.Second), midware.MiddlewareAuth(false), api.GETFollowerList) // 应用限流中间件, jwt鉴权中间件
+			relationAPI.GET("/friend/list/", midware.MiddlewareRateLimitWithRedis(rateLimit, time.Second), midware.MiddlewareAuth(true), api.GETFriendList)      // 应用限流中间件, jwt鉴权中间件(强制)
 		}
 
 		messageAPI := rootAPI.Group("message")
 		{
-			messageAPI.POST("/action/", midware.MiddlewareAuth(true), api.POSTMessage) // 应用jwt鉴权中间件(强制)
-			messageAPI.GET("/chat/", midware.MiddlewareAuth(true), api.GETMessageList) // 应用jwt鉴权中间件(强制)
+			messageAPI.POST("/action/", midware.MiddlewareRateLimitWithRedis(rateLimit, time.Second), midware.MiddlewareAuth(true), api.POSTMessage) // 应用限流中间件, jwt鉴权中间件(强制)
+			messageAPI.GET("/chat/", midware.MiddlewareRateLimitWithRedis(rateLimit, time.Second), midware.MiddlewareAuth(true), api.GETMessageList) // 应用限流中间件, jwt鉴权中间件(强制)
 		}
 	}
 
